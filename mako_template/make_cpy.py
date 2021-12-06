9aa21fe10bb0ad8003d0f6e3017b7380d22c2719
Alexander Salois <asalois.scholar@gmail.com>
2021-12-06 11:24:52 -0700

    copy to hyalite template

---
+++ mako_template/make_cpy.py
@@ -0,0 +1,10 @@
+from mako.template import Template
+my_scp_tmpl = Template(filename='cpy_hyalite_tmpl.txt')
+usr = 'v16b915'
+file_nm = '*slurm'
+to_path = '/mnt/lustrefs/scratch/' + usr + '/'
+
+file = open("to_hyalite.sh","w+")
+scp = my_scp_tmpl.render(user_id=usr, path=to_path, file_name=file_nm)
+file.writelines(scp)
+file.close()
