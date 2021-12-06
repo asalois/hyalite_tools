c3d07f43b89532f49b410e81c3339488190351f9
Alexander Salois <asalois.scholar@gmail.com>
2021-12-06 11:30:32 -0700

    make it less user specific

--- scripts/go_to_scratch.sh
+++ scripts/go_to_scratch.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Go to scratch and check queue
-cd /mnt/lustrefs/scratch/v16b915/
-squeue -u `whoami` -S id
+# Go to scratch   
+cd /mnt/lustrefs/scratch/ #please add your netid to save a cd
